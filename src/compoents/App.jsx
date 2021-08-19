b739e50fdc9d97b5d2c8cb222756d379423f0d36
Heterosis <39578132+Heterosis@users.noreply.github.com>
2021-08-19 13:23:18 +0800

    Add github repository link

--- src/compoents/App.jsx
+++ src/compoents/App.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import Game from './Game';
+import style from './App.module.css';
 
 const App = () => (
   <div>
+    <div className={`${style.ReverseRow}`}>
+      <a className={`${style.Link}`} href="https://github.com/Heterosis/Tic-Tac-Toe" target="_blank" rel="noreferrer">
+        <FontAwesomeIcon icon={['fab', 'github']} style={{ fontSize: 40 }} />
+      </a>
+    </div>
     <Game />
   </div>
 );
--- src/index.jsx
+++ src/index.jsx
@@ -2,5 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'css/index.css';
 import App from 'compoents/App';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+
+library.add(fab, fas);
 
 ReactDOM.render(<App name="react" />, document.getElementById('app'));
